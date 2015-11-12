a9f6d898f8412063ffc58a9d6df6d3223061dc5b
Maxipadula <padula.maximiliano@gmail.com>
2015-11-12 17:18:17 -0300

    Agregar vehiculos corregido!

--- TP FINAL/Web/php/ADMINISTRADOR/GESTION/VEHICULOS/validar_datos_vehiculos.php
+++ TP FINAL/Web/php/ADMINISTRADOR/GESTION/VEHICULOS/validar_datos_vehiculos.php
@@ -1,7 +1,7 @@
 <html> 
 	<?php include("vehiculos_datos.php"); ?>
  	<?PHP 
- 	session_start() ; 
+
  	 
 	$id_vehi =$_POST ["id_ve"]; 	 
  	$model =$_POST ["modelo"]; 
@@ -13,22 +13,22 @@
 	
 	$conexion = mysql_connect($puerto, $usuario,$password) or die("no conecta");
 	mysql_select_db ("tpFinal",$conexion) or die ("no db");
- 	 
- 	$consulta2= mysql_query(" SELECT id_modelo MODELO
-                               FROM modelo 
-                               WHERE descripcion = '".ucfirst($model)."' ") or die ("no q3"); 
+ 	
+		$consulta_id= mysql_query(" SELECT MAX( id_modelo ) ID
+                                        FROM modelo ") or die ("no query");
+                             
+			
+			$fila1 = mysql_fetch_assoc($consulta_id);
+			
+			$id_modelo= $fila1["ID"];
+			
+			$id_modelo +=100;
+			
+ 	$insert_modelo= mysql_query("insert into modelo (id_modelo, descripcion)
+								 values	('".$id_modelo."', '".ucfirst($model)."'); ") or die (mysql_error()); 
  	
 
-	$filas_afectadas_modelo = mysql_num_rows($consulta2);
 	
-	 if($filas_afectadas_modelo ==1){
-		
-		$fila2 = mysql_fetch_assoc($consulta2); 
-	
- 	    $model_id = $fila2["MODELO"]; 
-		
-	}
- 
  	 
  	 
  	//echo $id_mode; PARA VER QUE TRAEN
@@ -37,9 +37,8 @@
                                FROM marca
                                WHERE marca.descripcion = '".ucfirst($marc)."' ") or die ("no q2"); 
  	 
- 	$fila1 = mysql_fetch_assoc($consulta1); 
  							 
- 	$id_mar = $fila1["marca"]; 
+
  	$filas_afectadas_marca = mysql_num_rows($consulta1);
 	
 	if ($filas_afectadas_marca == 0){
@@ -58,9 +57,9 @@
 													values	('".$id_mar."', '".ucfirst($marc)."');");
 	}else if($filas_afectadas_marca ==1){
 		
-		$fila2 = mysql_fetch_assoc($consulta2); 
+		$fila2 = mysql_fetch_assoc($consulta1); 
 	
- 	    $id_mar = $fila2["MODELO"]; 
+ 	    $id_mar = $fila2["marca"]; 
 		
 	}	 					   
       
@@ -68,10 +67,10 @@
        
   
  	$insert_vehiculo = mysql_query("insert into vehiculo (id_vehiculo, id_modelo, id_marca , capacidad_carga) 
- 									values ('".$id_vehi."','".$model_id."','".$id_mar."','".$cap_carga."')  
+ 									values ('".$id_vehi."','".$id_modelo."','".$id_mar."','".$cap_carga."')  
  										    ;")or die (mysql_error()); 
  											 
-	header("Location: ".$vehiculos_datos."");
+	echo "CARGA EXITOSA";
  							 
   
  	?> 
